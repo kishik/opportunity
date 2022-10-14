c5d9a73dc7e20bc0e4950bf496462ee53d20fe4f
kishi <kishik.maxim@gmail.com>
2022-10-14 13:00:55 +0300

    adding of visualisation.py

--- visualisation.py
+++ visualisation.py
@@ -3,12 +3,21 @@ import streamlit as st
 import pandas as pd
 
 
-with open('transactions.json') as json_file:
-    data = json.load(json_file)
-    data = data['transactions']
-    data = data
-    result = pd.DataFrame.from_dict(data).T
-    result.index = result.index
-    print(result.dtypes)
+def visualisation():
+    with open('transactions.json') as json_file:
+        data = json.load(json_file)
+        data = data['transactions']
+        data = data
+        result = pd.DataFrame.from_dict(data).T
+        result.index = result.index.astype(int)
+        result['amount'] = result['amount'].astype(int)
+        result['date'] = pd.to_datetime(result['date'], format='%Y-%m-%dT%H:%M:%S')
+        result['account_valid_to'] = pd.to_datetime(result['account_valid_to'], format='%Y-%m-%d')
+        result['date_of_birth'] = pd.to_datetime(result['date_of_birth'], format='%Y-%m-%d')
+        result['passport_valid_to'] = pd.to_datetime(result['passport_valid_to'], format='%Y-%m-%d')
+        print(result.dtypes)
+        st.write(result)
 
-    st.write(result)
+
+if __name__ == '__main__':
+    visualisation()
