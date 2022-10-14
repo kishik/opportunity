62efba1723d4a283a95ab3e7cb90ddfd3e92ee28
kishi <kishi.maxim@gmail.com>
2022-10-14 20:08:48 +0300

    str(passport)

--- visualisation.py
+++ visualisation.py
@@ -3,12 +3,15 @@ import streamlit as st
 import pandas as pd
 
 
-def visualisation():
+@st.cache
+def load_data(file, nrows=None):
+    if nrows <= 0:
+        return pd.DataFrame()
     with open('transactions.json') as json_file:
         data = json.load(json_file)
         data = data['transactions']
-        data = data
         result = pd.DataFrame.from_dict(data).T
+        result = result[:nrows]
         result.index = result.index.astype(int)
         result['amount'] = result['amount'].astype(int)
         result['passport'] = result['passport'].astype(str)
@@ -16,9 +19,14 @@ def visualisation():
         result['account_valid_to'] = pd.to_datetime(result['account_valid_to'], format='%Y-%m-%d')
         result['date_of_birth'] = pd.to_datetime(result['date_of_birth'], format='%Y-%m-%d')
         result['passport_valid_to'] = pd.to_datetime(result['passport_valid_to'], format='%Y-%m-%d')
-        print(result.dtypes)
-        st.write(result)
+    return result
+
+
+def visualisation(file, nrows=10000):
+    st.header("Транзакции")
+    df = load_data(file, nrows)
+    st.dataframe(df)
 
 
 if __name__ == '__main__':
-    visualisation()
+    visualisation('transactions.json')
