c0771f91161146f98550fad7bb66444c68640423
Shaliko <shaliko9@yandex.ru>
2022-10-16 05:54:33 +0300

    test upload json file

--- backend/app.py
+++ backend/app.py
@@ -1,24 +1,31 @@
 import json
+import os
 
+from werkzeug.utils import secure_filename
 from flask import Flask, jsonify, request
 from backend.validate import Validate
 from backend.helper import Helper, get_fraud_transactions
 from backend.schemas.schemas import FAILED_VALIDATION
 
+UPLOAD_FOLDER = 'backend/uploads'
+
 
 app = Flask('FraudAPI')
 app.config['JSON_AS_ASCII'] = False
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 v = Validate()
 h = Helper()
 
 
-@app.route('/import_transations', methods=['POST'])
+@app.route('/import_transactions', methods=['POST'])
 def import_transations():
     file = request.files['file']
     if file:
-        if not v.allowed_file(file):
+        if not v.allowed_file(file.filename):
             return jsonify(FAILED_VALIDATION), 400
-        with open(file, 'r') as f:
+        filename = secure_filename(file.filename)
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        with open(filename, 'r') as f:
             data = json.loads(f.read())
     else:
         data = request.get_json()
